6edc863509dac14cf9feb797676e2b20aee4121c
Yasmine Makhlouf <yasmine.makhlouf@outlook.fr>
2024-05-14 00:56:26 +0100

    store commands in double char

--- main.c
+++ main.c
@@ -6,73 +6,72 @@
 /*   By: ymakhlou <ymakhlou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 18:10:16 by ymakhlou          #+#    #+#             */
-/*   Updated: 2024/05/13 22:36:32 by ymakhlou         ###   ########.fr       */
+/*   Updated: 2024/05/13 23:59:05 by ymakhlou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	add_back(t_list **stack_a, char **content)
+void    add_back(t_list **stack_a, char **content)
 {
-	t_list	*node;
-	t_list	*temp;
+    t_list    *node;
+    t_list    *temp;
 
-	node = malloc (sizeof(t_list));
-		puts ("add");
-	if (!node)
-		return ;
-	node->cmd = content;
-	node->next = NULL;
-	if ((*stack_a) == NULL)
-	{
-		*stack_a = node;
-		return ;
-	}
-	temp = *stack_a;
-	while (temp->next != NULL)
-		temp = temp->next;
-	temp->next = node;
+    node = malloc (sizeof(t_list));
+    if (!node)
+        return ;
+    node->cmd = content;
+    node->next = NULL;
+    if ((*stack_a) == NULL)
+    {
+        *stack_a = node;
+        return ;
+    }
+    temp = *stack_a;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = node;
 }
 
-void	store_cmd(char *input, t_list *cmd)
+void    store_cmd(char *input, t_list **list)
 {
-	char	**s;
-	int		i;
+    char    **s;
+    int        i;
 
-	i = 0;
-	s = ft_split(input, '|');
-	if (s == NULL)
-		return ;
-	while (s[i])
-	{
-		add_back(&cmd, &s[i]);
-		free(s[i]);
-		s[i] = NULL;
-		i++;
-	}
-	free(s);
+    i = 0;
+    s = ft_split(input, '|');
+    if (s == NULL)
+        return ;
+    while (s[i])
+    {
+        add_back(list, &s[i]);
+        i++;
+    }
+    free(s);
 }
 
-int	main ()
+int    main ()
 {
-	char	*input;
-	t_list	**cmd;
+    char    *input;
+    t_list    *list;
+    // int i = 0;
 
-	cmd = NULL;
-	while (1)
-	{
-		input = readline("Minishell$ ");
-		add_history(input);
-		syntax_error(input);
-		puts ("main");
-		store_cmd(input, *cmd);
-		t_list *current = cmd;  // Added this line
-        while (current->next)  // Changed this line
-        {
-			puts ("ok");
-            printf("%s\n", current->cmd);  // Changed this line
-            current = current->next;  // Changed this line
-        }
-        printf("%s\n", current->cmd);  // Changed this line
+    list = malloc(sizeof(t_list *));
+    if (!list)
+        return (1);
+	list = NULL;
+    while (1)
+    {
+        input = readline("Minishell$ ");
+        add_history(input);
+        syntax_error(input);
+        store_cmd(input, &list);
+		t_list *current = list;
+		while (current)
+		{
+			printf("cmd is %s\n", current->cmd[0]);
+			current = current->next;
+		}
+		free(input);
     }
 }
