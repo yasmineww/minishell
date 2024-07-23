c6b34f989c4f3e084227789e20dd804d0e7e71f1
marn11 <benchmarin5@gmail.com>
2024-07-23 14:06:43 +0100

    Fixed ambiguous

--- execution/src/redir.c
+++ execution/src/redir.c
@@ -6,7 +6,7 @@
 /*   By: mbenchel <mbenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 14:56:39 by mbenchel          #+#    #+#             */
-/*   Updated: 2024/07/22 23:29:50 by mbenchel         ###   ########.fr       */
+/*   Updated: 2024/07/23 14:05:17 by mbenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,16 +46,37 @@ int	handle_redirs(t_list *list, t_exp *exp)
 		}
 		else if (list->option[i] && !ft_strncmp(list->option[i], ">>", 2))
 		{
+			if (exp->ambiguous)
+			{
+				ft_error("Minishell:", list->option[i + 1], "ambiguous redirect\n");
+				exp->ambiguous = 0;
+				exp->status = 1;
+				return (1);
+			}
 			if (handle_append(list, i, exp))
 				return (1);
 		}
 		else if (list->option[i] && !ft_strncmp(list->option[i], ">", 1))
 		{
+			if (exp->ambiguous)
+			{
+				ft_error("Minishell:", list->option[i + 1], "ambiguous redirect\n");
+				exp->ambiguous = 0;
+				exp->status = 1;
+				return (1);
+			}
 			if (handle_redir_out(list, i, exp))
 				return (1);
 		}
 		else if (list->option[i] && !ft_strncmp(list->option[i], "<", 1))
 		{
+			if (exp->ambiguous)
+			{
+				ft_error("Minishell:", list->option[i + 1], "ambiguous redirect\n");
+				exp->ambiguous = 0;
+				exp->status = 1;
+				return (1);
+			}
 			if (handle_redir_in(list, i, exp))
 				return (1);
 		}
@@ -70,13 +91,6 @@ int	handle_redir_in(t_list *list, int i, t_exp *exp)
 	list->infile = -1;
 	if (list->option[i + 1])
 	{
-		if (exp->ambiguous)
-		{
-			ft_error("Minishell:", list->option[i + 1], "ambiguous redirect\n");
-			exp->ambiguous = 0;
-			exp->status = 1;
-			return (1);
-		}
 		list->infile = open(list->option[i + 1], O_RDONLY);
 		if (list->infile == -1)
 		{
@@ -95,13 +109,6 @@ int	handle_redir_out(t_list *list, int i, t_exp *exp)
 	list->outfile = -1;
 	if (list->option[i + 1])
 	{
-		if (exp->ambiguous)
-		{
-			ft_error("Minishell: ", list->option[i + 1], "ambiguous redirect\n");
-			exp->ambiguous = 0;
-			exp->status = 1;
-			return (1);
-		}
 		list->outfile = open(list->option[i + 1], O_RDWR
 				| O_CREAT | O_TRUNC, 0644);
 		if (list->outfile == -1)
@@ -121,13 +128,6 @@ int	handle_append(t_list *list, int i, t_exp *exp)
 	list->outfile = -1;
 	if (list->option[i + 1])
 	{
-		if (exp->ambiguous)
-		{
-			ft_error("Minishell: ", list->option[i + 1], "ambiguous redirect\n");
-			exp->ambiguous = 0;
-			exp->status = 1;
-			return (1);
-		}
 		list->outfile = open(list->option[i + 1], O_RDWR
 				| O_CREAT | O_APPEND, 0644);
 		if (list->outfile == -1)
